9f2659aea653127194e172a8d35c8e622ea5d37f
Jan Schwirtznik <jan.schwirtznik@unitb.de>
2025-01-13 22:00:42 +0100

    Add unit tests for CsvWeatherDataLoader
    Introduce tests to verify correct loading of weather data from a CSV file and proper exception handling for non-existing files. This ensures the data loader behaves as expected in both normal and error scenarios.

---
+++ src/test/java/de/bcxp/challenge/weather/CsvWeatherDataLoaderTest.java
@@ -0,0 +1,30 @@
+package de.bcxp.challenge.weather;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CsvWeatherDataLoaderTest {
+
+    @Test
+    void givenCsvFile() {
+        WeatherDataLoader weatherDataLoader = new CsvWeatherDataLoader(Path.of("./src/test/resources/weather.csv"));
+
+        List<WeatherData> data = weatherDataLoader.load();
+
+        assertEquals(1, data.size());
+        assertEquals(1, data.get(0).getDay());
+        assertEquals(88, data.get(0).getMaxTemperature());
+        assertEquals(59, data.get(0).getMinTemperature());
+    }
+
+    @Test
+    void givenNonExistingCsvFile() {
+        var exception = assertThrows(WeatherDataLoaderException.class, () -> new CsvWeatherDataLoader(Path.of("./src/test/resources/weathers.csv")).load());
+        assertEquals(NoSuchFileException.class, exception.getCause().getClass());
+    }
+}
