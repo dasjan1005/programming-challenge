67b78d9a7377bfbfd0fa44832a665262df916940
Jan Schwirtznik <jan.schwirtznik@unitb.de>
2025-01-13 21:59:10 +0100

    Add CsvWeatherDataLoader to handle weather data loading
    Introduced a new class implementing WeatherDataLoader to parse weather data from CSV files using OpenCSV. The class reads and processes the file to create a list of WeatherData objects, handling errors with a custom exception. This provides a structured way to load and manage weather data.

---
+++ src/main/java/de/bcxp/challenge/weather/CsvWeatherDataLoader.java
@@ -0,0 +1,38 @@
+package de.bcxp.challenge.weather;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CsvWeatherDataLoader implements WeatherDataLoader {
+
+    private static final int COLUMN_DAY = 0;
+    private static final int COLUMN_MAX_TEMPERATURE = 1;
+    private static final int COLUMN_MIN_TEMPERATURE = 2;
+
+    private final Path filename;
+
+    public CsvWeatherDataLoader(Path filename) {
+        this.filename = filename;
+    }
+
+    @Override
+    public List<WeatherData> load() throws WeatherDataLoaderException {
+        try {
+            try (Reader reader = Files.newBufferedReader(this.filename); CSVReader csvReader = new CSVReaderBuilder(reader).withSkipLines(1).build()) {
+                return csvReader.readAll().stream()
+                        .map(line -> new WeatherData(
+                                Integer.parseInt(line[COLUMN_DAY]),
+                                Integer.parseInt(line[COLUMN_MAX_TEMPERATURE]),
+                                Integer.parseInt(line[COLUMN_MIN_TEMPERATURE])
+                        )).collect(Collectors.toList());
+            }
+        } catch (Exception ex) {
+            throw new WeatherDataLoaderException("Cannot load weather data!", ex);
+        }
+    }
+}
