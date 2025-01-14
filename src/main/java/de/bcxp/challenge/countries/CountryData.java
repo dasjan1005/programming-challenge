6e5c2ac80ce81e778c0f4f9bfbba6949bb3e249a
Jan Schwirtznik <jan.schwirtznik@unitb.de>
2025-01-14 10:10:39 +0100

    Add method to calculate population density
    Introduced the `getPeoplePerSquareKilometer` method to compute the population density of a country.

--- src/main/java/de/bcxp/challenge/countries/CountryData.java
+++ src/main/java/de/bcxp/challenge/countries/CountryData.java
@@ -23,4 +23,8 @@ public class CountryData {
     public int getArea() {
         return area;
     }
+
+    public double getPeoplePerSquareKilometer() {
+        return population / (double) area;
+    }
 }
