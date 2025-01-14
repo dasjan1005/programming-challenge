7e6f22dc504ead487001b32d122a89c4e9e6aa61
Jan Schwirtznik <jan.schwirtznik@unitb.de>
2025-01-14 09:57:33 +0100

    Add custom exception for country data loading errors
    Introduced `CountryDataLoaderException` to handle errors specific to country data loading.

---
+++ src/main/java/de/bcxp/challenge/countries/CountryDataLoaderException.java
@@ -0,0 +1,8 @@
+package de.bcxp.challenge.countries;
+
+public class CountryDataLoaderException extends RuntimeException {
+
+    public CountryDataLoaderException(String message, Exception cause) {
+        super(message, cause);
+    }
+}
