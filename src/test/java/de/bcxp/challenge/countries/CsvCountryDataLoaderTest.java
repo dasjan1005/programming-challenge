26b1a82555120eefaa055e3f804a4c308898b26e
Jan Schwirtznik <jan.schwirtznik@unitb.de>
2025-01-14 10:00:52 +0100

    Add unit tests for CsvCountryDataLoader
    Introduced tests to validate the functionality of CsvCountryDataLoader. Tests include scenarios for valid CSV input, handling non-existent files, and different data formats to ensure correctness and robustness of the loader.

---
+++ src/test/java/de/bcxp/challenge/countries/CsvCountryDataLoaderTest.java
@@ -0,0 +1,48 @@
+package de.bcxp.challenge.countries;
+
+import de.bcxp.challenge.weather.CsvWeatherDataLoader;
+import de.bcxp.challenge.weather.WeatherData;
+import de.bcxp.challenge.weather.WeatherDataLoader;
+import de.bcxp.challenge.weather.WeatherDataLoaderException;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CsvCountryDataLoaderTest {
+
+
+    @Test
+    void givenCsvFile() {
+        CountryDataLoader countryDataLoader = new CsvCountryDataLoader(Path.of("./src/test/resources/countries.csv"));
+
+        List<CountryData> data = countryDataLoader.load();
+
+        assertEquals(27, data.size());
+        assertEquals("Austria", data.get(0).getName());
+        assertEquals(8926000, data.get(0).getPopulation());
+        assertEquals(83855, data.get(0).getArea());
+    }
+
+    @Test
+    void givenNonExistingCsvFile() {
+        var exception = assertThrows(WeatherDataLoaderException.class, () -> new CsvWeatherDataLoader(Path.of("./src/test/resources/country.csv")).load());
+        assertEquals(NoSuchFileException.class, exception.getCause().getClass());
+    }
+
+    @Test
+    void givenDifferentDataFormat() {
+
+        CountryDataLoader countryDataLoader = new CsvCountryDataLoader(Path.of("./src/test/resources/countries.csv"));
+
+        List<CountryData> data = countryDataLoader.load();
+
+        assertEquals(27, data.size());
+        assertEquals("Croatia", data.get(3).getName());
+        assertEquals(4036355, data.get(3).getPopulation());
+        assertEquals(56594, data.get(3).getArea());
+    }
+}
