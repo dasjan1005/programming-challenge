544f4842a4fe22d82dce3c7f3dc5ab0999adcf80
Jan Schwirtznik <jan.schwirtznik@unitb.de>
2025-01-14 09:57:11 +0100

    Add CountryData class to represent country information

---
+++ src/main/java/de/bcxp/challenge/countries/CountryData.java
@@ -0,0 +1,26 @@
+package de.bcxp.challenge.countries;
+
+public class CountryData {
+
+    private final String name;
+    private final int population;
+    private final int area;
+
+    public CountryData(String name, int population, int area) {
+        this.name = name;
+        this.population = population;
+        this.area = area;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public int getArea() {
+        return area;
+    }
+}
